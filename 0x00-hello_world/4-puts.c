ae5a390ee8bc779134e83156cf50e0d782312902
King Joseph <endurancechery@gmail.com>
2023-08-11 22:22:26 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+* main - Aprogram that prints a line with puts function
+* Return: 0 (success)
+*/
+int main(void)
+{
+puts("\"programing is like building a sultilingual puzzle");
+return (0);
+}
+
