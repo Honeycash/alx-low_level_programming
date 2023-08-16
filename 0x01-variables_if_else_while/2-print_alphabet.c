fb1736e4483e202903206fa373cb9e40019b31a6
King Joseph <endurancechery@gmail.com>
2023-08-15 19:08:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Discription:'Check for number if it is positive or negative'
+*Return: always 0 (success)
+*/
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchat('\n');
+return (0);
+}
